95523361357e9ffa3b2a006583772599fd986062
arifulislamarif <arif14arif15@gmail.com>
2021-01-26 22:53:28 +0600

    calculator created done

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,131 @@
 <template>
-    <div id="app">
-        <compBookListOne :book="books"></compBookListOne>
-        <compBookListTwo :book="books"></compBookListTwo>
+    <div id="app" class="calculator">
+       <div class="display">{{ current ||'0' }}</div>
+       <div @click="clear" class="btn ac">AC</div>
+       <div @click="sign" class="btn">+/-</div>
+       <div @click="percent" class="btn">%</div>
+       <div @click="divide" class="btn operator">÷</div>
+       <div @click="append('7')" class="btn">7</div>
+       <div @click="append('8')" class="btn">8</div>
+       <div @click="append('9')" class="btn">9</div>
+       <div @click="times" class="btn operator">✕</div>
+       <div @click="append('4')" class="btn">4</div>
+       <div @click="append('5')" class="btn">5</div>
+       <div @click="append('6')" class="btn">6</div>
+       <div @click="minus" class="btn operator">-</div>
+       <div @click="append('1')" class="btn">1</div>
+       <div @click="append('2')" class="btn">2</div>
+       <div @click="append('3')" class="btn">3</div>
+       <div @click="add" class="btn operator">+</div>
+       <div @click="append('0')" class="btn zero">0</div>
+       <div @click="dot" class="btn">.</div>
+       <div @click="equal" class="btn operator">=</div>
     </div>
 </template>
 
 <script>
-import compBookListOne from "./components/BookListOne"
-import compBookListTwo from "./components/BookListTwo"
 export default {
-    data(){
-        return{
-            books: [
-              {name: "Harry Potter", price: 100},
-              {name: "Lord of The Rings", price: 150},
-              {name: "Night Circus", price: 90},
-              {name: "Lost World", price: 200}
-            ]
+    data() {
+        return {
+           current: '',
+           operator: null, 
+           previous: null, 
+           operatorClicked: false, 
         }
     },
-    components:{
-      compBookListOne,
-      compBookListTwo
-    }
+    methods: {
+        clear(){
+           this.current = ''
+        },
+        sign(){
+           this.current = this.current.charAt(0) === '-' ? this.current.slice(1) : `-${this.current}`;
+        },
+        percent(){
+           this.current = `${parseFloat(this.current) / 100}`
+        },
+        append(number){
+            if (this.operatorClicked) {
+                this.current = '';
+                this.operatorClicked = false;
+            }
+            this.current = `${this.current}${number}`;
+        },
+        dot(){
+            if (this.current.indexOf('.') === -1) {
+                this.append('.');
+            }
+        },
+        setPrevious(){
+            this.operatorClicked = true;
+            this.previous = this.current;
+        },
+        divide(){
+            this.operator = (a, b) => a / b;
+            this.setPrevious();
+        },
+        times(){
+            this.operator = (a, b) => a * b;
+            this.setPrevious();
+        },
+        minus(){
+            this.operator = (a, b) => b - a ;
+            this.setPrevious();
+        },
+        add(){
+            this.operator = (a, b) => a + b;
+            this.setPrevious();
+        },
+        equal(){
+            this.current = `${this.operator(
+                parseFloat(this.current),
+                parseFloat(this.previous),
+            )}`;
+
+            this.previous = null;
+        },
+    },
 }
 </script>
+
+<style>
+    .calculator{
+        width: 350px;
+        margin: 0 auto;
+        font-size: 30px;
+        font-weight: bold;
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-auto-rows: minmax(50px,auto);
+    }
+    .display{
+        grid-column: 1/5;
+        background-color: #333;
+        color: white;
+        text-align: right;
+        padding-right: 10px;
+    }
+    .zero{ 
+        grid-column: 1/3;
+    }
+    .btn{
+        background-color: #f2f2f2;
+        border: 1px solid #999;
+        font-size: 30px;
+        font-weight: bold;
+    }
+    .ac{
+        background-color: rgb(192, 51, 51);
+        border: 1px solid rgb(192, 51, 51);
+        color: white;
+    }
+    .ac:hover{
+        color: white;
+    }
+    .operator{
+        background-color: orange;
+        color: white;
+    }
+    .operator:hover{
+        color: white;
+    }
+</style>
